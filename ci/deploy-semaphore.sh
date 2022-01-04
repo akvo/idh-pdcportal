497f0055fc1abc4dbcfe03cb83d0fb50c691e3fc
dedenbangkit <mail@dedenbangkit.com>
2022-01-04 13:55:05 +0700

    [#5] Fix missing space

--- ci/deploy-semaphore.sh
+++ ci/deploy-semaphore.sh
@@ -10,7 +10,7 @@ sudo chown "${USER}:" . -R
 
 echo "Deploying site..."
 
-if [[ "${TRAVIS_BRANCH}" != "main" &&  "${TRAVIS_BRANCH}" != "develop"]]; then
+if [[ "${TRAVIS_BRANCH}" != "main" && "${TRAVIS_BRANCH}" != "develop" ]]; then
     exit 0
 fi
 
