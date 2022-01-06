708231e5cf6f1a3d33707d7f036da262f9e669ef
wayangalihpratama <wgprtm@gmail.com>
2022-01-06 14:13:55 +0800

    [#4] Add new country geojson for echart map
    - prettier

--- resources/js/components/PrivateRoute.js
+++ resources/js/components/PrivateRoute.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
 import { Redirect, Route } from "react-router-dom";
 
-const PrivateRoute = ({component:Component, ...rest}) => {
-    console.log('PrivateRoute.js', rest);
-    return (
-        <Route {...rest} render={props => (
-            rest.isLogin
-                ? (rest.redirect) 
-                    ? <Redirect to={rest.redirect} />
-                    : <Component {...props} />
-                : <Redirect to="/login" />
-        )} />
-    );
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        rest.isLogin ? (
+          rest.redirect ? (
+            <Redirect to={rest.redirect} />
+          ) : (
+            <Component {...props} />
+          )
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
 };
 
 export default PrivateRoute;
