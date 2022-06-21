2a2733b9b9eac14dc788c27354eaf4323900e682
dedenbangkit <mail@dedenbangkit.com>
2022-06-21 16:24:58 +0700

    [#15] add or substract for custom stackbar

--- resources/js/charts/options/CustomStackBar.js
+++ resources/js/charts/options/CustomStackBar.js
@@ -9,7 +9,6 @@ import {
 } from "../chart-options.js";
 import { sumBy, sortBy } from "lodash";
 
-// const bgcolors = ['#00b3b6', '#f2f2f2', '#ddd'];
 const bgcolors = ["#68BED1", "#f2f2f2", "#ddd"];
 export const CustomStackBar = (title, data, compare = false) => {
   // if (!data.length) {
@@ -23,10 +22,21 @@ export const CustomStackBar = (title, data, compare = false) => {
     header: [title, "Count"],
     data: tableData,
   };
-  let legends = data.map((x) => x.name);
-  let total = [{ name: "total", value: sumBy(data, "value") }];
-  let series = data.map((x, i) => {
-    let percent = (x.value / total[0].value) * 100;
+  const legends = data.map((x) => x.name);
+  const total = [{ name: "total", value: sumBy(data, "value") }];
+  data = data.map((x) => ({
+    ...x,
+    percent: parseInt(((x.value / total[0].value) * 100).toFixed(0)),
+  }));
+  const percentage_total = sumBy(data, "percent");
+  if (percentage_total !== 100) {
+    const addup = percentage_total - 100;
+    data = data.map((x, xi) => ({
+      ...x,
+      percent: xi ? x.percent : x.percent - addup,
+    }));
+  }
+  const series = data.map((x, i) => {
     return {
       name: x.name,
       type: "bar",
@@ -45,7 +55,7 @@ export const CustomStackBar = (title, data, compare = false) => {
         borderWidth: 2,
       },
       barWidth: "11%",
-      data: [percent.toFixed(2)],
+      data: [x.percent],
     };
   });
   return {
